36ad6ead14f1b14539227f79a82576331f2c1340
Colin But <colinbut@users.noreply.github.com>
2016-04-03 20:19:18 +0100

    still first draft - just some inital thoughts

--- src/main/java/com/mycompany/hangman/Hangman.java
+++ src/main/java/com/mycompany/hangman/Hangman.java
@@ -1,13 +1,73 @@
 package com.mycompany.hangman;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Hello world!
+ * Hangman
+ *
+ * @author colin
  *
  */
 public class Hangman
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private String word;
+    private List<Character> alphabet = new ArrayList<>();
+
+    private static final String letters = "abcdefghijklmnopqrstuvvwxyz";
+
+
+    public Hangman(String word) {
+        this.word = word;
+        initialiseAlphabet();
+    }
+
+    private void initialiseAlphabet() {
+        for (int i = 0; i < letters.length() - 1; i++) {
+            alphabet.add(letters.charAt(i));
+        }
+    }
+
+
+    private boolean hasWordContainLetter(char character) {
+        return word.indexOf(character) != -1;
+    }
+
+    public String guessLetter(char character) {
+        if (hasWordContainLetter(character)) {
+            System.out.format("Word: %s has letter: %s%n", word, character);
+        }
+        alphabet.remove(new Character(character));
+        return getPartialGuessedWord(character);
+    }
+
+
+    public List<Character> getAvailableCharactersFromAlphabet() {
+        return alphabet;
+    }
+
+    private String getPartialGuessedWord(Character guessedCharacter) {
+        String wordToBeGuessed = word;
+        char[] wordCharArray = word.toCharArray();
+        for (int i = 0; i < wordCharArray.length; i++) {
+            if (!Character.valueOf(wordCharArray[i]).equals(guessedCharacter)) {
+                wordToBeGuessed = wordToBeGuessed.replace(wordCharArray[i], '*');
+            }
+        }
+        return wordToBeGuessed;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.format("Incorrect number of arguments supplied %d: ", args.length);
+        }
+
+        //Hangman hangman = new Hangman(args[0]);
+
+        Hangman hangman = new Hangman("Spotify");
+        String result = hangman.guessLetter('A');
+        System.out.println(result);
     }
 }
--- src/test/java/com/mycompany/hangman/HangmanTest.java
+++ src/test/java/com/mycompany/hangman/HangmanTest.java
@@ -1,17 +1,44 @@
 package com.mycompany.hangman;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple Hangman.
+ *
+ * @author colin
  */
 public class HangmanTest {
 
-    private Hangman hangman = new Hangman();
+    private Hangman hangman;
+
+    @Before
+    public void setup() {
+        hangman = new Hangman("Spotify");
+    }
+
+
 
     @Test
-    public void testPlayHangman() {
-        hangman.play('A');
+    public void testGuessLetter() {
+        Assert.assertEquals("*******", hangman.guessLetter('a'));
+        Assert.assertEquals("**o****", hangman.guessLetter('o'));
+        Assert.assertEquals("*po****", hangman.guessLetter('p'));
+        Assert.assertEquals("*po****", hangman.guessLetter('g'));
+        Assert.assertEquals("*pot***", hangman.guessLetter('t'));
+        Assert.assertEquals("*poti**", hangman.guessLetter('i'));
+
+        System.out.println(hangman.getAvailableCharactersFromAlphabet());
+
+    }
+
+    @Test
+    public void testGetAvailableCharactersFromAlphabet() {
+
+        hangman.guessLetter('S');
+
+        System.out.println(hangman.getAvailableCharactersFromAlphabet());
     }
 
 }
